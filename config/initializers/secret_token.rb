e19b83e96d129ca0294a9a90827ce80b1be4d917
Syunsuke Mihara <mihara@swlab.cs.okayama-u.ac.jp>
2011-11-02 14:35:52 +0900

    DTBのRails3への移植開始．

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesktopBookmark::Application.config.secret_token = '9a6fdf74531835edd8e9dc7690758838cb92b4580e387faeb9365fcd9d2dae3998cd90ec69d20b817437e7e08f0172a55425bf7e5de387a424dd5dddca4beae9'
